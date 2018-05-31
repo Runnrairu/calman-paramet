6ca4debf0336a8cb26cd12b9c0f96e8c99a94e2f
宮本来夏 <runnrairu@gmail.com>
2018-05-31 22:10:57 +0900

    iroiro

--- calman-paramet.py
+++ calman-paramet.py
@@ -1,17 +1,17 @@
 ﻿# -*- coding: utf-8 -*-
 import numpy as np
-import random
+
 import matplotlib.pyplot as plt
 
 
 def a(theta):
-    return 3*theta+0.01
+    return theta
 
 def b(theta):
-    return theta*theta
+    return 2*theta
 
 def c(theta):
-    return 2
+    return 0.5
     
 def r(theta):
     x = a(theta)*a(theta)+b(theta)*b(theta)*c(theta)*c(theta)/(sigma_o*sigma_o)
@@ -20,92 +20,92 @@ def r(theta):
 def gamma(theta):
     return r(theta)-a(theta)
 
-def m_t_y(y):
-    m_y = [0]*(m+1)
+def m_t_y(y,Y,i,T):
+    
     
     r_y=r(y)
     a_y=a(y)
     gamma_y = r_y-a_y
-    m_y[0]=np.random.normal(0,pow(gamma_y*gamma_y*sigma_o*sigma_o/(2*a_y),0.5))
-    for i in range(m):
-        #e_r = pow(e,-float(i)/t*r_y)
-        #m_y_etr[i+1] = m_y_etr[i]+pow(e,float(i)/t*r_y)*gamma_y*(Y[i+1]-Y[i])
-        #m_y[i+1] = m_y_etr[i+1]*e_r
-        m_y[i+1] = m_y[i]-(a_y+gamma_y)*delta_t+gamma_y*(Y[i+1]-Y[i])
+    step=int(T*nt)
+    m_y = [0]*(step+1)
+    for t in range(step):
+        m_y[t+1] = m_y[t]+(gamma_y-a_y)*m_y[t]+gamma_y*(Y[i][t+1]-Y[i][t])
     return m_y
-    
 
-def L(y):#尤度関数を書く 
-    m_y=m_t_y(y)
+def L(y,Y,i,T):#対数尤度関数を書く 
+    m_y=m_t_y(y,Y,i,T+1)#m_yの計算
     sumx=0
     sumt=0
-    
-    for i in range(m):      
-        sumx += m_y[i+1]*(Y[i+1]-Y[i])
-        sumt += m_y[i+1]*m_y[i+1]*delta_t  
-    return sumx/(sigma_o*sigma_o)-sumt/(2*sigma_o*sigma_o)
+    step = int((T+1)*nt)
+    for t in range(step):      
+        sumx +=  (m_y[t]-m_theta1[i][t])*(Y[i][t+1]-Y[i][t])
+        sumt +=  (m_y[t]*m_y[t]-m_theta1[i][t]*m_theta1[i][t])*delta_t
+    return sumx/(step*sigma_o*sigma_o)-sumt/(step*2*sigma_o*sigma_o)
 
-def p(y):#事前分布
-    if y<1 and y>0:
-        return 1
-    else:
-        return 0
-
-def q(y):#事後分布（定数除く）
-    return p(y)*L(y)
 
 
 e=2.71828182846
- #時刻は1000分割、100秒
-T=20.0
-t=1000#分割係数
-n=int(T)*t
-theta=0.5#random.random()#[0,1]の一葉乱数
-X =[0]*(n+1)
-Y =[0]*(n+1)
-delta_t = 1.0/t
-theta1=0.5
-sigma = np.power(delta_t,0.5)
+ #時刻は1秒1000分割、10秒
+T=10.0
+nt=1000#分割係数
+n=int(T)*nt
+test_case = 1000 #いくつサンプルパスを作るか
+X =[[0 for t in range(n+1)]for i in range(test_case)]
+Y =[[0 for t in range(n+1)]for i in range(test_case)]
+delta_t = 1.0/nt
+theta=np.random.rand()+0.5#0.5~1.5の乱数
+sigma = np.power(delta_t,0.5)#ブラウン運動の分散
 sigma_o =1.0 #最初にわかってる分散
 a_theta = a(theta)
 b_theta = b(theta)
 c_theta = c(theta)
 r_theta = r(theta)
+gamma_theta= r_theta-a_theta
+print(theta)
+theta1=1.0
 a_theta1 = a(theta1)
 b_theta1 = b(theta1)
 c_theta1 = c(theta1)
 r_theta1 = r(theta1)
-gamma_theta1 = r_theta1-a_theta1
-print(theta)
-gamma_theta = gamma(theta)
-dW_y =np.random.normal(0,sigma,n)
-dW_x =np.random.normal(0,sigma,n)
+gamma_theta1= r_theta1-a_theta1
+dW_y =np.random.normal(0,sigma,(test_case,n))
+dW_x =np.random.normal(0,sigma,(test_case,n))
 
-m_theta1 = [0]*(n+1)
+m_theta1 = [[0 for i in range(int(T))]for t in range(test_case)]
 #sample = 10000#サンプル数
 #MC = [[0 for i in range(sample)]for j in range(int(T))] 
 
 
-for i in range(n):
-    deltaX=-a_theta*X[i]*delta_t+b_theta*dW_x[i]
-    deltaY=c_theta*X[i]*delta_t + sigma_o*dW_y[i]
-    X[i+1] = X[i]+deltaX
-    Y[i+1] = Y[i]+deltaY
-    m_theta1[i+1] = m_theta1[i]-(a_theta1+gamma_theta1)*m_theta1[i]*delta_t+gamma_theta1*deltaY
+for i in range(test_case):
+    n_theta1 = [0]*(n+1)
+    for t in range(n):
+        deltaX=-a_theta*X[i][t]*delta_t+b_theta*dW_x[i][t]
+        deltaY=c_theta*X[i][t]*delta_t + sigma_o*dW_y[i][t]
+        X[i][t+1] = X[i][t]+deltaX
+        Y[i][t+1] = Y[i][t]+deltaY
+        n_theta1[t+1] = n_theta1[t]+pow(e,float(t/nt)*r_theta1)*deltaY
+    m_theta1[i]=m_t_y(theta1,Y,i,T-1)
+opt_theta = [0]*(1001)#区間を1000分し、カウントする
+for cutT in range(int(T)-1):
+    for i in range(test_case):
+        y=0.5
+        minL=-1000000000#Lの最大値。とにかく小さくとる
+        miny=-1 #argmin_y L 
+        for j in range(1001):#区間の1000分割
+            y += float(j)/1000
+            L_y=L(y,Y,i,cutT+1)
+            if minL > L_y:
+                miny= j
+                minL= L_y
+        opt_theta[miny] += 1
 
-maxim = [0]*(int(T))
-
-for i in range(int(T)):#T/10秒ご
+        
     
-    m = (i+1)*t
-    y_i=np.array([0 for j in range(100)])
-    print(m)
-    for j in range(100):
-        
-        y_i[j] = L(float(j)/100)
-        
-    maxim[i] = np.argmax(y_i)
-plt.plot(maxim)
+    
+    
+
+
+plt.plot(opt_theta)
 plt.show()
     
     
